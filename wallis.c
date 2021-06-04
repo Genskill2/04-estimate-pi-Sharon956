aa0ff6526a8d51ad5997d8e7a4bf843a51f96167
Sharon956 <84805843+Sharon956@users.noreply.github.com>
2021-06-04 12:36:42 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,11 +23,18 @@ int main(void) {
     }
   }
 }
-float wallis_pi(int count){
-    float pi = 1.0;
-    for (int i = 1; i < count; i++){
-        float num = 4.0 * i * i;
-        pi *= num / (num - 1);
-    }
-    return pi * 2;
+
+float wallis_pi(int i){
+
+float s=1;
+float d=1;
+float c=1;
+ for(int n=1;n<=i;n++){
+ 
+ c=4*n*n;
+ d=c/(c-1);
+ s=s*d;
  }
+ float x=2*s;
+return x;
+}
